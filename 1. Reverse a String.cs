b98dce450c6e64e1bb34202f6d7de01745bfa73e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-09 23:08:01 +0200

    Add files via upload

---
+++ 1. Reverse a String.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _1._Reverse_a_String
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+        }
+    }
+}
