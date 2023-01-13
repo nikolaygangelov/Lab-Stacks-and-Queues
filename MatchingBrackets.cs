f37aecee4fca5daa255f9db8ed9113f2e0208b42
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-13 16:27:49 +0200

    Add files via upload

---
+++ MatchingBrackets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string expression = Console.ReadLine();
+			Stack<int> openBracketsInexes = new Stack<int>();
+
+			
+			for (int i = 0; i < expression.Length; i++)
+			{
+				char currentLetter = expression[i];
+				
+				if (currentLetter == '(')
+				{
+					openBracketsInexes.Push(i);
+				}
+
+				if (currentLetter == ')')
+				{
+						int openBracketIndex = openBracketsInexes.Pop();
+					string subExpression = expression.Substring(openBracketIndex, i - openBracketIndex + 1);
+                    Console.WriteLine(subExpression);
+  			    }
+
+			}
+		}
+    }
+}
